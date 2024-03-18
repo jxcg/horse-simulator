c1137aaa5ccfa39032354aadc8f4caa084e5619d
jxcg <154761377+jxcg@users.noreply.github.com>
2024-03-18 22:51:47 +0000

    Pasted code from old Horse class to new Horse

--- part1/src/Horse.java
+++ part1/src/Horse.java
@@ -1,76 +1,69 @@
-
 /**
- * Write a description of class Horse here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ *
+ * The Horse class is in charge of instantiating the Horse object,
+ * represented by a Unicode character, name,
+ * with details about the Horse object, such as the distance travelled of the individual Horse
+ * plus its confidence rating.
+ *
+ * Joshua Cameron Ng
+ * version 1.0 - 12/03/24
  */
-public class Horse
-{
-    //Fields of class Horse
-    
-    
-      
-    //Constructor of class Horse
-    /**
-     * Constructor for objects of class Horse
-     */
-    public Horse(char horseSymbol, String horseName, double horseConfidence)
-    {
-       
+public class Horse {
+    // Fields of class
+    private String horseName; /* e.g., Pippi Longstocking */
+    private char horseSymbol; /* represents horse char unicodeChar = '\u0041'; */
+    private int horseTravelled; /* whole number */
+    private boolean horseFell;
+    private double horseConfidence; /* 0-1 */
+
+    // Constructor of class Horse
+
+    public Horse(char horseSymbol, String horseName, double horseConfidence) {
+        this.horseSymbol = horseSymbol;
+        this.horseName = horseName;
+        this.horseConfidence = horseConfidence;
     }
-    
-    
-    
-    //Other methods of class Horse
-    public void fall()
-    {
-        
-    }
-    
-    public double getConfidence()
-    {
-        
-    }
-    
-    public int getDistanceTravelled()
-    {
-        
-    }
-    
-    public String getName()
-    {
-        
-    }
-    
-    public char getSymbol()
-    {
-        
-    }
-    
-    public void goBackToStart()
-    {
-        
-    }
-    
-    public boolean hasFallen()
-    {
-        
+    // Other methods of class Horse
+
+    // Accessor Methods
+    public double getConfidence() {
+        return this.horseConfidence;
     }
 
-    public void moveForward()
-    {
-        
+    public int getDistanceTravelled() {
+        return this.horseTravelled;
     }
 
-    public void setConfidence(double newConfidence)
-    {
-        
+    public String getName() {
+        return this.horseName;
     }
-    
-    public void setSymbol(char newSymbol)
-    {
-        
+
+    public char getSymbol() {
+        return this.horseSymbol;
     }
-    
+    public boolean hasFallen() {
+        return this.horseFell;
+    }
+
+    // Mutator Methods
+
+    public void fall() {
+        this.horseFell = true;
+    }
+
+    public void goBackToStart() {
+        this.horseTravelled = 0; /* resets horse back to start of race */
+    }
+
+    public void moveForward() {
+        this.horseTravelled = this.horseTravelled + 1;
+    }
+
+    public void setConfidence(double newConfidence) {
+        this.horseConfidence = newConfidence;
+    }
+
+    public void setSymbol(char newSymbol) {
+        this.horseSymbol = newSymbol;
+    }
 }
