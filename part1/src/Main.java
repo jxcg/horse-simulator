63390ceb87b0c9683bb63a48af735e604b6bdc3b
jxcg <154761377+jxcg@users.noreply.github.com>
2024-03-22 12:20:16 +0000

    CL: Merging into main updated changes

--- part1/src/Main.java
+++ part1/src/Main.java
@@ -10,9 +10,7 @@
 class Main {
     public static void main(String[] args) {
         Horse zappyHorse = new Horse('a', "Zappy-Horse", 0.46);
-
         Horse dataHorse = new Horse('a',"Data-Horse", 0.5);
-
         Horse serverHorse = new Horse('a',"Server-Horse", 0.5);
         zappyHorse.setSymbol('♕');
         dataHorse.setSymbol('♞');
